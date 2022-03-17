52e5f88e60accebeb0b9895a18de8b7af8275ac3
Kayden Ahn <kayden_ahn@intuit.com>
2022-03-16 23:22:46 -0700

    clean code

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-######### Import your libraries #######
 import dash
 from dash import html
 from dash import dcc
@@ -8,7 +7,7 @@ import plotly as py
 import plotly.graph_objs as go
 
 
-###### Define your variables #####
+# Global variables
 tabtitle = 'Titanic!'
 color1='#92A5E8'
 color2='#8E44AD'
@@ -17,70 +16,70 @@ sourceurl = 'https://www.kaggle.com/c/titanic'
 githublink = 'https://github.com/plotly-dash-apps/304-titanic-dropdown'
 
 
-###### Import a dataframe #######
-df = pd.read_csv("https://raw.githubusercontent.com/austinlasseter/plotly_dash_tutorial/master/00%20resources/titanic.csv")
-df['Female']=df['Sex'].map({'male':0, 'female':1})
-df['Cabin Class'] = df['Pclass'].map({1:'first', 2: 'second', 3:'third'})
-variables_list=['Survived', 'Female', 'Fare', 'Age']
+# Load data for anlysis
+# https://github.com/austinlasseter/plotly_dash_tutorial/blob/master/00%20resources/titanic.csv
+df = pd.read_csv("https://raw.githubusercontent.com/austinlasseter"
+                 "/plotly_dash_tutorial/master/00%20resources/titanic.csv")
+df['Female'] = df['Sex'].map({'male': 0, 'female': 1})
+df['Cabin Class'] = df['Pclass'].map({1: 'first', 2: 'second', 3: 'third'})
+variables_list = ['Survived', 'Female', 'Fare', 'Age']
 
-########### Initiate the app
+# app server config
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 app.title=tabtitle
 
-####### Layout of the app ########
+# app component layout
 app.layout = html.Div([
     html.H3('Choose a continuous variable for summary statistics:'),
     dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in variables_list],
-        value=variables_list[0]
-    ),
+        id = 'dropdown',
+        options = [{'label': i, 'value': i} for i in variables_list],
+        value=variables_list[0]),
     html.Br(),
-    dcc.Graph(id='display-value'),
+    dcc.Graph(id = 'display-value'),
     html.A('Code on Github', href=githublink),
     html.Br(),
     html.A("Data Source", href=sourceurl),
 ])
 
+# callback
+@app.callback(
+    Output('display-value', 'figure'),
+    [Input('dropdown', 'value')])
+def display_value(option):
+    grouped_mean = df.groupby(['Cabin Class', 'Embarked'])[option].mean()
+    results = pd.DataFrame(grouped_mean)
 
-######### Interactive callbacks go here #########
-@app.callback(Output('display-value', 'figure'),
-              [Input('dropdown', 'value')])
-def display_value(continuous_var):
-    grouped_mean=df.groupby(['Cabin Class', 'Embarked'])[continuous_var].mean()
-    results=pd.DataFrame(grouped_mean)
     # Create a grouped bar chart
-    mydata1 = go.Bar(
+    data1 = go.Bar(
         x=results.loc['first'].index,
-        y=results.loc['first'][continuous_var],
+        y=results.loc['first'][option],
         name='First Class',
         marker=dict(color=color1)
     )
-    mydata2 = go.Bar(
+    data2 = go.Bar(
         x=results.loc['second'].index,
-        y=results.loc['second'][continuous_var],
+        y=results.loc['second'][option],
         name='Second Class',
         marker=dict(color=color2)
     )
-    mydata3 = go.Bar(
+    data3 = go.Bar(
         x=results.loc['third'].index,
-        y=results.loc['third'][continuous_var],
+        y=results.loc['third'][option],
         name='Third Class',
         marker=dict(color=color3)
     )
 
-    mylayout = go.Layout(
+    layout = go.Layout(
         title='Grouped bar chart',
         xaxis = dict(title = 'Port of Embarkation'), # x-axis label
-        yaxis = dict(title = str(continuous_var)), # y-axis label
+        yaxis = dict(title = str(option)), # y-axis label
 
     )
-    fig = go.Figure(data=[mydata1, mydata2, mydata3], layout=mylayout)
-    return fig
+    return go.Figure(data=[data1, data2, data3], layout=layout)
 
 
-######### Run the app #########
 if __name__ == '__main__':
     app.run_server(debug=True)
