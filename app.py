09d83050a363899f376f18db2a208eacf10dd32d
Kayden Ahn <kayden_ahn@intuit.com>
2022-03-18 12:12:27 -0700

    update pie chart

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ from dash import dcc
 from dash.dependencies import Input, Output, State
 import pandas as pd
 import plotly as py
+import plotly.express as px
 import plotly.graph_objs as go
 from plotly.subplots import make_subplots
-
+import matplotlib.pyplot as plt
 
 # Global variables
 tabtitle = 'Drinks world!'
@@ -27,9 +28,9 @@ df['Alcohol Level'] = pd.cut(
     labels=['Low', 'Medium', 'High'])
 df['Alcohol Level'] = df['Alcohol Level'].fillna('Low')
 df.loc[df['country'].isna(), 'continent'] = 'NA'
-
-
+df['beer_servings'] = df['beer_servings'].fillna(0.0)
 print(df)
+
 # df['Female'] = df['Sex'].map({'male': 0, 'female': 1})
 # df['Cabin Class'] = df['Pclass'].map({1: 'first', 2: 'second', 3: 'third'})
 variables_list = [
@@ -55,6 +56,8 @@ app.layout = html.Div([
     dcc.Graph(id='output1'),
     html.Br(),
     dcc.Graph(id='output2'),
+    html.Br(),
+    dcc.Graph(id='scatter1'),
     html.A('Code on Github', href=githublink),
     html.Br(),
     html.A("Data Source", href=sourceurl),
@@ -95,7 +98,7 @@ def display_value(option):
         textposition='auto')
 
     layout = go.Layout(
-        title='Grouped bar chart',
+        title='Drink consumption by continent bar chart',
         xaxis=dict(title='Continent'), # x-axis label
         yaxis=dict(title=str(option)),) # y-axis label
 
@@ -109,39 +112,39 @@ def display_value(option):
 def display_value(option):
     grouped_mean = df.groupby(['Alcohol Level', 'continent'])[option].mean()
     results = pd.DataFrame(grouped_mean)
-    print(results)
     rows = 1
     cols = 3
     specs = [[{'type': 'domain'}] * cols] * rows
     fig = make_subplots(
         rows=1,
         cols=3,
-        specs=specs)
+        specs=specs,)
 
     # Create a grouped bar chart
     fig.add_trace(go.Pie(
         labels=results.loc['Low'].index,
         values=results.loc['Low'][option],
-        name="Low Alcohol",
-        hoverinfo="label+percent+name"), 1, 1)
+        title="Low Alcohol",
+        hoverinfo="label+percent+name",
+        ), 1, 1)
 
     fig.add_trace(go.Pie(
         labels=results.loc['Medium'].index,
         values=results.loc['Medium'][option],
-        name="Medium Alcohol",
+        title="Medium Alcohol",
         hoverinfo="label+percent+name"), 1, 2)
 
     fig.add_trace(go.Pie(
         labels=results.loc['High'].index,
         values=results.loc['High'][option],
-        name="High Alcohol",
+        title="High Alcohol",
         hoverinfo="label+percent+name"), 1, 3)
 
-    fig.update_traces(hole=0.4, hoverinfo='label+percent+name')
+    fig.update_traces(hole=0.7, hoverinfo='label+percent+name')
 
     layout = go.Layout(
-        title='Grouped bar chart',
-        xaxis=dict(title='Continent'), # x-axis label
+        title='Drink consumption by continent PIE chart',
+        xaxis=dict(title='Continent'),
         yaxis=dict(title=str(option)),
         annotations=[
             dict(text='Low', x=0.12, y=0.6, font_size=100, showarrow=False),
@@ -149,6 +152,24 @@ def display_value(option):
             dict(text='High', x=0.18, y=0.6, font_size=20, showarrow=False)])
 
     return go.Figure(data=fig, layout=layout)
+
+
+# # callback
+# @app.callback(
+#     Output('scatter1', 'figure'),
+#     [Input('dropdown', 'value')])
+# def display_value(option):
+#     data=go.Scatter(
+#         x=df['country'],
+#         y=df['total_litres_of_pure_alcohol'],
+#         mode='markers',
+#         marker_color=df['total_litres_of_pure_alcohol'],
+#         marker_size=df['total_litres_of_pure_alcohol'],
+#         text=df['country']) # hover text goes here
+#     layout = go.Layout(
+#         title='Drink consumption by continent scatter chart',
+#     )
+#     return go.Figure(data=data, layout=layout)
 
 
 if __name__ == '__main__':
